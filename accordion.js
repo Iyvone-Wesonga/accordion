046baadb93eed2bdf08b5e470590fafc78d1a954
Iyvone Wesonga <iyvonewesonga385@gmail.com>
2022-11-17 08:54:49 +0300

    accordion for faqs or any additional data
    Will definitely reuse this in any pages I will make

---
+++ accordion.js
@@ -0,0 +1,8 @@
+//variables and fetching
+
+const accordion=document.getElementsByClassName('content-container')
+for(var i=0; i<accordion.length; i++){
+    accordion[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+    })
+}
